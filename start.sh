c7ddebb55fdf24821a7bd626d8626e0075aa050b
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-08-11 12:48:07 -0600

    Pequeña modificacion
    Pequeña modificacion

--- start.sh
+++ start.sh
@@ -6,4 +6,7 @@ mysql -u root <<-EOSQL
     CREATE DATABASE IF NOT EXISTS db_ventas;
 EOSQL
 
+# Esto asegura que Apache busque index.php primero
+echo "DirectoryIndex index.php index.html" >> /etc/apache2/apache2.conf
+
 apachectl -D FOREGROUND
