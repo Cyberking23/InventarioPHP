a55bba5e96ee3d1b364a9f203a7385c4df13fbba
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-08-27 11:33:10 -0600

    Update
    Update

--- config/db.php
+++ config/db.php
@@ -1,18 +1,13 @@
 <?php
-// Tomamos los datos desde las variables de entorno de Railway
 $host     = getenv("MYSQLHOST");
 $port     = getenv("MYSQLPORT");
 $user     = getenv("MYSQLUSER");
 $password = getenv("MYSQLPASSWORD");
 $database = getenv("MYSQLDATABASE");
 
-// Conexión con mysqli
 $conexion = new mysqli($host, $user, $password, $database, $port);
 
-// Verificar conexión
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
-
 echo "Conexión exitosa 🚀";
-?>
