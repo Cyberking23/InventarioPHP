837533f8f8d1102198dccf0f3dedd9587e82166c
Carlos Eduardo Merino Ventura <102163019+Cyberking23@users.noreply.github.com>
2025-08-27 10:59:59 -0600

    Modificacion de BD
    Modificacion de BD

--- config/db.php
+++ config/db.php
@@ -1,6 +1,18 @@
 <?php
+// Tomamos los datos desde las variables de entorno de Railway
+$host     = getenv("MYSQLHOST");
+$port     = getenv("MYSQLPORT");
+$user     = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$database = getenv("MYSQLDATABASE");
 
-$conexion = new mysqli("db", "root", "root", "db_ventas");
+// Conexión con mysqli
+$conexion = new mysqli($host, $user, $password, $database, $port);
 
+// Verificar conexión
+if ($conexion->connect_error) {
+    die("Error de conexión: " . $conexion->connect_error);
+}
 
+echo "Conexión exitosa 🚀";
 ?>
